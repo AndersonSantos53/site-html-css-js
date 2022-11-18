06d02db28913c9f0744c2269376612c2c03da529
Anderson Santos <112710292+AndersonSantos53@users.noreply.github.com>
2022-11-18 12:18:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function handleClickReverse(){
+    document.querySelector('#fotoe').innerHTML = '<nav class="dis"> <img class="foto" src="https://thumbs2.imgbox.com/2a/a2/B7F18hTg_t.png"> </nav>'
+
+}
+function handleClick(){
+    document.querySelector('#fotoe').innerHTML = '<nav class="dis"> <img class="foto" src="https://thumbs2.imgbox.com/d6/c8/imnT2sTF_t.png"> </nav>'
+
+}
